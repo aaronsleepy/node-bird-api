5eaae9aa8a3ebd7f6552e8d61ec7ef1c9d3e69e3
aaronsleepy <aaron@kmong.com>
2021-09-25 16:52:28 +0900

    task: Add jwt

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -13,4 +15,23 @@ exports.isNotLoggedIn = (req, res, next) => {
       const message = encodeURIComponent('Already logged in');
       res.redirect(`/?error=${message}`);
     }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if ('TokenExpiredError' === error.name) {
+      return res.status(419).json({
+        code: 419,
+        message: 'Token is expried',
+      });
+    }
+
+    return res.status(401).json({
+      code: 401,
+      message: 'Token is not vaild',
+    });
+  }
 };
